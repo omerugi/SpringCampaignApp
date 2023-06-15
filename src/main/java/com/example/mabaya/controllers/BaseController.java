e8f7fe353c75522c06682d636f169660a43bc00d
omerugi <omer.rugi@operative.com>
2023-06-15 13:51:36 +0300

    Basic controller

--- src/main/java/com/example/mabaya/controllers/BaseController.java
+++ src/main/java/com/example/mabaya/controllers/BaseController.java
@@ -1,9 +1,8 @@
 package com.example.mabaya.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class BaseController {
     @GetMapping
